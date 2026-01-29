4e07b06a9cb39165898490fee121ae1ff5aec05e
Krithik Duraisamy <krithik.duraisamy@gmail.com>
2026-01-29 16:43:02 -0700

    started java

---
+++ firstlabemacs/IntroLab.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class IntroLab {
+
+	public static List<Integer> test() {
+		return null;
+	}
+
+	public static String teststr() {
+		return "IntroLab []";
+	}
+
+	public static void main(String[] args) {
+		System.out.println(test());
+		System.out.println(teststr());
+	}
+}
