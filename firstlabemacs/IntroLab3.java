df0aaf9fc2d6f99f26fed28e0a2f803227cd4675
Krithik Duraisamy <krithik.duraisamy@gmail.com>
2026-01-30 18:27:27 -0700

    still need to finish the recursive part

--- firstlabemacs/IntroLab3.java
+++ firstlabemacs/IntroLab3.java
@@ -1,21 +1,31 @@
 public class IntroLab3 {
 
-    public static 
+	public static void makeChange(int amount) {
+		if (amount > 100) {
+			System.err.println("Can't be more than 100!");
+		}
+		System.out.println("Change for " + amount + ":");
+		int[] coinArr = { 25, 10, 5, 1 };
+		int[] rtnArr = { 0, 0, 0, 0 };
+		String[] strArr = { "  quarters: ", "  dimes: ", "  nickels: ", "  pennies: " };
 
-    public static void makeChange(int amount) {
-	if (amount > 100) {
-	    System.err.println("Can't be more than 100!");
+		int ii = 0;
+		while (ii != rtnArr.length) {
+			int currCoin = coinArr[ii];
+			if (amount >= currCoin) {
+				rtnArr[ii] = amount / currCoin;
+				amount -= currCoin * rtnArr[ii];
+			}
+			ii++;
+		}
+		for (int jj = 3; jj >= 0; jj--) {
+			System.out.println(strArr[jj] + rtnArr[jj]);
+		}
 	}
-	String rtnStr = "Change for " + amount + ":";
-	int[] rtnArr = {25, 10, 5, 1};
-
-	while 
-	
-	
-	return rtnStr;
-    }
 
 	public static void main(String[] args) {
-	
+		makeChange(25);
+		makeChange(97);
 	}
+
 }
