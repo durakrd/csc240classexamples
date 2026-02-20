931f3e085d453a4290efe02815612da1a283c3fd
Krithik Duraisamy <krithik.duraisamy@gmail.com>
2026-02-20 03:59:02 -0700

    again

---
+++ assignments/c/mypractice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "mymath.h"
+
+int main(void) {
+  int radius;
+  double area;
+  printf("Enter the radius: ");
+  scanf("%d", &radius);  
+  area = PI * square(radius);
+  printf ("The area of a circle with radius %d is %.2f\n", radius, area);
+  return 0;
+}
