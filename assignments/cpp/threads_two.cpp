78d3a5c029b38ff372fb8f4333db771ed1ceb160
durakrd <krithik.duraisamy@gmail.com>
2026-03-24 12:21:21 -0700

    Finished Multi-Threading Lab so it uses array instead of vector. Function paramater declaration should be the same as mian() declaration.

--- assignments/cpp/threads_two.cpp
+++ assignments/cpp/threads_two.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
-#include <vector>
 #include <mutex>
 #include <thread>
 
 using namespace std;
+
+constexpr int ROWS = 5;
+constexpr int COLS = 5;
 
 thread_local int thrd_counter;
 thread_local int total_count = 0;
 
 mutex writeMutex;
 
-void addCol(vector<vector<int>> matrix, int colNum) {
-  for (auto vec : matrix) {
-    total_count += vec[colNum];
+void addCol(int matrix[ROWS][COLS], int colNum) {
+  for (int kk = 0; kk < ROWS; kk++) {
+    total_count += *(((*matrix) + kk) + colNum);
+    // total_count += matrix[kk][colNum]; //both are acceptable
   }
   unique_lock<mutex> lock(writeMutex);
   cout << "Column " << colNum + 1 << ": " << total_count << endl;
 }
 
 int main(void) {
-  constexpr int ROWS = 5;
-  constexpr int COLS = 5;
 
-  vector<vector<int>> matrix(ROWS, vector<int>(COLS));
+  int matrix[ROWS][COLS];
   int ii, jj;
 
   thread t[COLS];
@@ -39,11 +40,12 @@ int main(void) {
   for (ii = 0; ii < COLS; ii++) {
     thrd_counter = ii;
     t[ii] = thread(addCol, matrix, thrd_counter);
+    t[ii].join();
   }
 
-  for (int ww = 0; ww < COLS; ww++) {
-    t[ww].join();
-  }
+  // for (int ww = 0; ww < COLS; ww++) {
+  //   t[ww].join();
+  // }
 
   cout << "Finished" << endl;
 
