93840da5b880ac35ebab2211803d04956d4c37e4
Krithik Duraisamy <krithik.duraisamy@gmail.com>
2026-04-18 22:10:27 -0700

    Finished Prolog Lab 1

--- assignments/prolog/practice1.pl
+++ assignments/prolog/practice1.pl
@@ -1,4 +1,5 @@
-% state/2 (country, name).
+
+%state/2(country, name).
 state(unitedstates, arizona).
 state(unitedstates, california).
 state(unitedstates, newmexico).
@@ -15,7 +16,10 @@ state(mexico, durango).
 state(mexico, veracruz).
 state(mexico, bajacalifornia).
 
-% stateinfo/3 (Name, Population, Area-sq-mi)
+isState(State) :- state(_, State).
+biggerstate(State1, State2) :- stateinfo(State1, _, X), stateinfo(State2, _, Y), X > Y.
+
+% stateinfo/3(Name, Population, Area-sq-mi)
 stateinfo(chihuahua, 3400000, 96000).
 stateinfo(sonora, 2700000, 69000).
 stateinfo(coahuila, 2700000, 58000).
@@ -23,7 +27,8 @@ stateinfo(california, 34000000, 163000).
 stateinfo(texas, 21000000, 269000).
 stateinfo(arizona, 5000000, 111990).
 
-% show/2 (Title, Year, Rating)
+% show/2(Title, Year, Rating).
+
 show(seinfeld, 1989, 8.9).
 show(the_xfiles, 1993, 8.7).
 show(friends, 1994, 8.9).
@@ -39,3 +44,21 @@ show(shadow_and_bone, 2019, 7.7).
 show(the_mandalorian, 2019, 8.8).
 show(loki, 2021, 8.3).
 show(mr_robot, 2015, 8.6).
+
+good_show(Show) :- show(Show, _, Rating), Rating >= 8.9.
+
+same_year(Show1, Show2) :- show(Show1, Year1, _), show(Show2, Year2, _), Year1 == Year2, Show1 \= Show2.
+
+quad(X, Y) :- Y is 4 * X.
+
+bloop(Z, Y) :- Z >= 0, Y is Z * 2.
+bloop(W, Y) :- W < 0, Y is W / 2.
+
+mult(X,Y,Z) :- Z is X * Y.
+
+bigger(X,Y,X) :- X >= Y.
+bigger(Y,X,X) :- X >= Y.
+
+
+absol(X,X) :- X > 0.
+absol(X,Y) :- Y is X * -1, X < 0.
